f8c6b9ba6d7a1fa7fb48fff687aaec5b49616f57
Gilles Taupenas <gilles.taupenas@gmail.com>
2015-10-07 18:02:55 +0200

    manage key removal by union and intersection on Redis lists

--- Client/RedisClient.php
+++ Client/RedisClient.php
@@ -61,4 +61,49 @@ class RedisClient
 
         return $nbAddedElements;
     }
+
+    /**
+     * Remove the keys matching the given tags.
+     * - By intersection if one dimension array is provided.
+     * - If 2-dimensions array is provided
+     *   - sUion is applied between 1st dimension keys
+     *   - in the 1st dimension key, sInter is applied between the 2nd dimension keys
+     *
+     * Example :
+     *  The given array :   array( array('tag0', 'tag1'), array('tag2', 'tag3'))
+     *  It becomes :        sunion( sinter('tag0', 'tag1'), sinter('tag2', 'tag3') )
+     *
+     * If no multidimension is set to the tags array, keys removal will be managed by intersection.
+     * array('tag0', 'tag1') will become array( array('tag0', 'tag1') )
+     *
+     * @param   array   $tags
+     * @return  array   Returns an array ('attempted' => array($keys), 'really_deleted' => (int) )
+     */
+    public function removeKeysFromTags(array $tags)
+    {
+        //return $this->connection->sMembers('site-2');
+        if (isset($tags[0]) && !is_array($tags[0])) {
+            $tags = array($tags);
+        }
+
+        $mergedKeys = array();
+        foreach($tags as $intersectExpression) {
+            $currentKeys = count($intersectExpression) > 1 ?
+                $this->executeEval('sinter', $intersectExpression)
+                : $this->connection->sMembers($intersectExpression[0]);
+
+            $mergedKeys = is_array($currentKeys) ? array_merge($mergedKeys, $currentKeys) : $mergedKeys;
+
+        }
+        // execute key removal on unique keys and return them
+        $mergedKeys = array_unique($mergedKeys);
+        $nbDeleted = $this->executeEval('del', $mergedKeys);
+
+        return array('attempted' => $mergedKeys, 'really_deleted' => $nbDeleted);
+    }
+
+    protected function executeEval($operation, $arguments)
+    {
+        return $this->connection->eval("return redis.call('$operation', '" . implode("','", $arguments) . "') ");
+    }
 }
