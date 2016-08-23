1ff2583ca325a00d27dd42a108305ed70d11a3ce
itkg-yfarci <yannick.farcy@businessdecision.com>
2016-08-23 17:57:27 +0200

    Update HttpCache.php

--- HttpCache/HttpCache.php
+++ HttpCache/HttpCache.php
@@ -21,6 +21,14 @@ class HttpCache extends BaseHttpCache
         if (false === $redisConnection = $this->envOptions['redis_connection']) {
             throw new \RuntimeException("Please define redis_connection parameter for the Itkg\\CombinedHttpCache.");
         }
+        
+        $tagsToExclude = array();
+        if (isset($this->envOptions['redis_tags_exclude']) && !is_array($this->envOptions['redis_tags_exclude'])){
+            throw new \RuntimeException("Please define redis_tags_exclude parameter for the Itkg\\CombinedHttpCache with an array.");
+        }elseif(isset($this->envOptions['redis_tags_exclude'])){
+            $tagsToExclude = $this->envOptions['redis_tags_exclude'];
+        }
+        
         return new Store($redisConnection, $this->cacheDir ?: $this->kernel->getCacheDir().'/http_cache');
     }
 }
